bd2fc827b563136997ea583946d51d072f1ff8d0
kkawai <kkawai2@gmail.com>
2026-06-30 16:45:45 -0700

    set platform details in user defaults for native sdk to report
    Signed-off-by: jeroen-tap <jeroen@tapresearch.com>

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -18,8 +18,6 @@ allprojects {
     repositories {
         google()
         mavenCentral()
-        maven("https://artifactory.tools.tapresearch.io/artifactory/tapresearch-android-sdk/")
-        mavenLocal()
     }
 }
 
@@ -77,8 +75,7 @@ dependencies {
     testImplementation("org.mockito:mockito-core:5.0.0")
 
     // required by tap research sdk
-    // implementation("com.tapresearch:tapsdk:3.8.0--beta04-local")
-    implementation("com.tapresearch:tapsdk:3.7.3--rc2")
+    implementation("com.tapresearch:tapsdk:3.7.3--rc3")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
     implementation("androidx.lifecycle:lifecycle-process:2.6.1")
     implementation("com.google.android.gms:play-services-ads-identifier:18.1.0")
