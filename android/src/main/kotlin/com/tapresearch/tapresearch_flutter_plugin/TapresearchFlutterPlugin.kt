b5dcddb1d8456a06e695829e62cd7e6616dcf92f
jeroen-tap <jeroen@tapresearch.com>
2026-08-18 19:05:19 -0700

    Update package version to 3.8.0--rc1, set iOS SDK to 3.8.0--rc1, set Android SDK to 3.8.0--rc2

--- android/src/main/kotlin/com/tapresearch/tapresearch_flutter_plugin/TapresearchFlutterPlugin.kt
+++ android/src/main/kotlin/com/tapresearch/tapresearch_flutter_plugin/TapresearchFlutterPlugin.kt
@@ -36,7 +36,7 @@ class TapresearchFlutterPlugin : FlutterPlugin, MethodCallHandler {
     companion object {
 
         // edit to be same as tapresearch_flutter_plugin version in pubspec.yaml!
-        const val VERSION = "3.8.0--rc0"
+        const val VERSION = "3.8.0--rc1"
     }
 
     private var applicationContext: WeakReference<Context>? = null
