b79a61d6ab8723d8edf6ada5a8cdf859ebbe0c9b
jeroen-tap <62305778+jeroen-tap@users.noreply.github.com>
2026-08-18 21:47:10 -0700

    Update package version to 3.8.0--rc1, set iOS SDK to 3.8.0--rc1, set Android SDK to 3.8.0--rc2 (#15)

--- ios/Classes/TapresearchFlutterPlugin.swift
+++ ios/Classes/TapresearchFlutterPlugin.swift
@@ -107,7 +107,7 @@ public final class TapresearchFlutterPlugin: NSObject, FlutterPlugin,
     TapResearchSDKDelegate, TapResearchRewardDelegate,
     TapResearchQuickQuestionDelegate, TapResearchSurveysDelegate {
 
-	@objc public static let packageVersion: String = "3.8.0--rc0"
+	@objc public static let packageVersion: String = "3.8.0--rc1"
 
     private var channel: FlutterMethodChannel!
     private var hasRewardCallback = false
